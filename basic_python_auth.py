06d7c613ddaa479afa8504504914304cef97e969
null <75458417+xqnullxp@users.noreply.github.com>
2020-12-03 20:19:56 -0600

    Add files via upload
    this is a basic python username and password script easy to use user and pass is null but you can change accordingly in the code.

---
+++ basic_python_auth.py
@@ -0,0 +1,21 @@
+#wrote by null
+#if you find this delete it.
+
+import os, time, threading 
+
+username = input ('\x1b[0;35m user: ')
+password = input ('pass: ')
+
+if username == '\x1b[0;35m null':
+    print('\x1b[0;35m lmao')
+time.sleep(2)
+os.system('cls')
+
+if password == 'null':
+    print('\x1b[0;35m checking credentials')
+time.sleep(3)
+os.system('cls')
+
+print("\x1b[0;35m loading") 
+time.slee(5)
+os.system('cls')
